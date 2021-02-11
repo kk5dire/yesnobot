294f4b314951630141c7a2cc5ec6512daa33772c
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-10 22:02:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on("ready", () => {
-    console.log("Bot Online")
+    console.log("Bot Online");
+    client.user.setActivity('To Messages, and no.help', { type: 'LISTENING' });
+    client.user.setStatus('dnd');
 })
 client.on("message", msg => {
     if (msg.author == client.user) {
@@ -16,12 +18,36 @@ client.on("message", msg => {
    
 })
 client.on('message', message => {
-    if (message.content === '+ping') {
+    if (message.content === 'no.ping') {
       const embed = new Discord.MessageEmbed
       .setTitle("Responce Ping")
       .addField(`My Latency is ${Date.now() - message.createdTimestamp}ms.`)
       .addField(`API Latency is ${Math.round(client.ws.ping)}ms`)
       .setFooter("yesnobot")
+      message.channel.send(embed);
     }
   });
+  client.on('message', message => {
+      if (message.content === 'no.limit') {
+          const embed = new Discord.MessageEmbed
+          .setTitle("Limiting Bot To Channels")
+          .setImage('https://i.ibb.co/84rNWGr/sendperms.png')
+          .addField('Make sure the check is clicked on channels you want the bot to talk in')
+          .addField('And make sure the X is checked in channels you dont want the bot to talk in')
+          .setFooter("yesnobot")
+          message.channel.send(embed);
+
+      }
+  })
+  client.on('message', message => {
+      if (message.content === 'no.help') {
+          const embed = new Discord.MessageEmbed
+          .setTitle("Help Menu")
+          .addField('no.help - shows this menu')
+          .addField('no.ping - shows the bot ping')
+          .addField('no.limit shows how to limit the bot to certain channels')
+          .setFooter('yesnobot')
+          message.channel.send(embed);
+      }
+  })
 client.login('ODA5MjQ4MjY4NzQzNzM3MzY0.YCSVLg.jXFgFj-SJjziEhafTd-jGohVrvE');

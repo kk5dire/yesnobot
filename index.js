490f6c3422a527b1c733aa3f2b8aa709f4756cc0
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-11 13:48:27 -0500

    fix eval desc in no.help

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ client.on('message', message => {
           embed.addField('no.limit ', '-shows how to limit the bot to certain channels')
           embed.addField('no.invite', '-Invite the bot to another server')
           embed.addField('no.leave', '<:ShieldWarning:746952447095996467>**REQUIRES: Manage_Guild** : Removes the bot from the entire server')
-          embed.addField('no.evaluation', '<:ShieldWarning:746952447095996467>**REQUIRES: Manage_Guild** : Evaluate Javascript Code \(Does not interact with server\)')
+          embed.addField('no.evaluation', '<:ShieldWarning:746952447095996467>**REQUIRES: BOT_Mangagment** : Evaluate Javascript Code \(Does not interact with server\)')
           embed.setFooter('yesnobot')
           message.channel.send(embed);
       }

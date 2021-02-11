bcd0cac9ebf405517c73f1f27c7e3a55c5bf0d5f
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-10 21:30:29 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.client();
+client.on("ready", () => {
+    console.log("Bot Online")
+})
+client.on("message", msg => {
+    if (msg.content === "yes","Yes","Yep","yep") {
+        msg.reply("Nope")
+    } else if (msg.content === "No","nope","no","Nope") {
+        msg.channel.send("Yes")
+    }
+})
+client.login('ODA5MjQ4MjY4NzQzNzM3MzY0.YCSVLg.jXFgFj-SJjziEhafTd-jGohVrvE');

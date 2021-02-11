e9bb6d6da06981ddbacbf4cef757dd6c7ab8bd4d
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-10 21:38:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ client.on("ready", () => {
     console.log("Bot Online")
 })
 client.on("message", msg => {
-    msg.channel.send("No")
+    if (message.author == client.user) {
+        return;
+    } else if (message.author.bot) {
+        return;
+    } else {
+        msg.channel.send("No");
+    }
+    
+    
+   
 })
 client.login('ODA5MjQ4MjY4NzQzNzM3MzY0.YCSVLg.jXFgFj-SJjziEhafTd-jGohVrvE');

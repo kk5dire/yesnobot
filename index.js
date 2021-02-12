446e8145c8c76a37d332b764084b22c10287db03
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-12 04:09:01 -0500

    fix line help2

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ client.on('message', message => {
         embed.addField('no.leave', '<:ShieldWarning:746952447095996467>**REQUIRES: Manage_Guild** : Removes the bot from the entire server')
         embed.addField('no.evaluation', '<:ShieldWarning:746952447095996467>**REQUIRES: BOT_Mangagment** : Evaluate Javascript Code \(Does not interact with server\)')
         embed.addField('no.snipe', '<:Image_not_found:754483783545127023> **INDEV: For Codebase**: A command to reveal deleted and edited messages/images')
-        embed.addField('**End of Page**'), linetext
+        embed.addField('**End of Page**', linetext)
         embed.addField('no.help#', newhelpdes)
         embed.setFooter(`Page 2/${maxhelppages}`)
         message.channel.send(embed)
